7cbdd054ccb09d7cc57418fd1b2bf5110c46df8b
Danil Kebkal <kebkal.danil@gmail.com>
2022-09-29 19:31:49 +0300

    split fix

--- random/array.d.ts
+++ random/array.d.ts
@@ -1,2 +1,2 @@
-export declare type AccesibleType = "number" | "int" | `string:${string}-${string}` | `number:${number}-${number}` | `int:${number}-${number}`;
+export declare type AccesibleType = "number" | "int" | `string:${string}-${string}` | `number:${number | `-${number}`}-${number}` | `int:${number | `-${number}`}-${number}`;
 export declare function of<T extends AccesibleType>(type: T, length: number): T extends `string${string}` ? string[] : number[];
