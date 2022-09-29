5fb47ff01001bc97e70d7f39dfbd70751efb7b4c
Danil Kebkal <kebkal.danil@gmail.com>
2022-09-29 18:19:47 +0300

    random array export fix

--- src/random/array.ts
+++ src/random/array.ts
@@ -3,69 +3,72 @@ import * as int from "./int";
 import { split } from "../split";
 
 export type AccesibleType = "number" | "int" | `string:${string}-${string}` | `number:${number}-${number}` | `int:${number}-${number}`;
-export const array = {
-  of(type: AccesibleType, length: number) {
-    const [gtype, srange] = split(type, ":", { minLength: 2, maxLength: 2 });
-    const srangeDeli = srange?.indexOf("-", 1);
-    let fromAsString: string | undefined;
-    let toAsString: string;
-    if (srangeDeli !== void 0 && srangeDeli !== -1) {
-      fromAsString = srange.slice(0, srangeDeli);
-      toAsString = srange.slice(srangeDeli + 1);
-    } else {
-      toAsString = srange;
+
+export function of<T extends AccesibleType>(type: T, length: number): T extends `string${string}` ? string[] : number[];
+export function of(type: AccesibleType, length: number) {
+  const [gtype, srange] = split(type, ":", { minLength: 1, maxLength: 2 });
+  const srangeDeli = srange?.indexOf("-", 1);
+  let fromAsString: string | undefined;
+  let toAsString: string;
+  if (srangeDeli !== void 0 && srangeDeli !== -1) {
+    fromAsString = srange.slice(0, srangeDeli);
+    toAsString = srange.slice(srangeDeli + 1);
+  } else {
+    toAsString = srange;
+  }
+  switch (gtype) {
+    case "number": {
+      const result = <number[]>[];
+      //@ts-expect-error
+      const from = +fromAsString || 0;
+      const to = +(toAsString ?? 1);
+      for (let i = 0; i < length; i++) {
+        result[i] = number.inRange(from, to);
+      }
+      return result;
     }
-    switch (gtype) {
-      case "number": {
-        const result = <number[]>[];
-        //@ts-expect-error
-        const from = +fromAsString || 0;
-        const to = +(toAsString ?? 1);
-        for (let i = 0; i < length; i++) {
-          result[i] = number.inRange(from, to);
-        }
-        return result;
+    case "int": {
+      const result = <number[]>[];
+      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+      /**@ts-ignore */
+      const from = fromAsString | 0;
+      const to = +(toAsString ?? Number.MAX_SAFE_INTEGER);
+      for (let i = 0; i < length; i++) {
+        result[i] = int.inRange(from, to);
       }
-      case "int": {
-        const result = <number[]>[];
-        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-        /**@ts-ignore */
-        const from = fromAsString | 0;
-        const to = +(toAsString ?? Number.MAX_SAFE_INTEGER);
-        for (let i = 0; i < length; i++) {
-          result[i] = int.inRange(from, to);
-        }
-        return result;
-      }
-      case "string": {
-        const result = <string[]>[];
-        if (fromAsString === undefined) {
-          fromAsString = "";
-        }
-        for (let i = 0; i < length; i++) {
-          const length = int.inRange(fromAsString.length, toAsString.length + 1);
-          let str = "";
-          for (let stri = 0; stri < length; stri++) {
-            str += String.fromCodePoint(
-              int.inRange(
-                fromAsString.codePointAt(stri)!,
-                toAsString.codePointAt(stri)!,
-              ),
-            );
-          }
-          result[i] = str;
-        }
-        return result;
-      }
+      return result;
     }
-  },
-} as {
-  of(
-    type: "number" | "int" | `number:${number}-${number}` | `int:${number}-${number}`,
-    length: number
-  ): number[];
-  of(
-    type: `string:${string}-${string}`,
-    length: number
-  ): string[];
+    case "string": {
+      const result = <string[]>[];
+      if (fromAsString === undefined) {
+        fromAsString = "";
+      }
+      for (let i = 0; i < length; i++) {
+        const length = int.inRange(fromAsString.length, toAsString.length + 1);
+        let str = "";
+        for (let stri = 0; stri < length; stri++) {
+          str += String.fromCodePoint(
+            int.inRange(
+              fromAsString.codePointAt(stri)!,
+              toAsString.codePointAt(stri)!,
+            ),
+          );
+        }
+        result[i] = str;
+      }
+      return result;
+    }
+  }
+  throw new Error();
 };
+  /*,
+} as {
+of(
+  type: "number" | "int" | `number:${number}-${number}` | `int:${number}-${number}`,
+  length: number
+): number[];
+of(
+  type: `string:${string}-${string}`,
+  length: number
+): string[];
+};*/
